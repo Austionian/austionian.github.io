f2b6d9c7ce0a4396c8a334bd07af97fa672e5c56
Austin <austin@r00ks.io>
2022-01-03 22:31:57 -0600

    refine footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 const Email = require('../../assets/email.svg')
+const Github = require('../../assets/github.svg')
 
 const Footer = () => {
     return (
@@ -14,9 +15,18 @@ const Footer = () => {
                     />
                 </a>
             </p>
-            <p>
-                © {new Date().getFullYear()}
-            </p>
+            <br />
+            <div className='index-heading'>
+                <p>
+                    <a href="https://github.com/Austionian/austionian.github.io">
+                        <Github className='social-icon' />
+                        {" "}Source Code
+                    </a>
+                </p>
+                <p>
+                    © {new Date().getFullYear()}
+                </p>
+            </div>
         </div>
     )
 }
